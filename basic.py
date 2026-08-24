3c669b784af2342240812b56159fd32f6add0b22
sanathecoder <sanasana33377737@gmail.com>
2026-08-25 01:59:36 +0500

    slicing

--- basic.py
+++ basic.py
@@ -1,3 +1,9 @@
-for i in range(0,11):
-    if i % 2 == 0:
-        print(i ,"is even")
+# loop
+for i in range(1,31):
+    if (i % 3 == 0):
+        print(i )
+
+# List
+marks = [20,30,40,50,60,70]
+print(marks[-1])
+print(marks[-3:])
